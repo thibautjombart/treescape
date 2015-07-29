b7b8f4a8cfc275e7616961426a1915ff9f189782
Michelle Kendall <m.kendall@imperial.ac.uk>
2015-07-29 14:47:02 +0100

    Update metrics.R
    Neatening up the documentation

--- R/metrics.R
+++ R/metrics.R
@@ -108,7 +108,7 @@ pen.edge.treematch <- cmpfun(pen.edge.treematch)
 #' @author  Michelle Kendall \email{michelle.louise.kendall@@gmail.com}
 #'
 #' @param tr1 an object of the class \code{phylo} 
-#' @param lambda a number in [0,1] which specifies the extent to which topology (default, with lambda=0)  or branch lengths (lambda=1) are emphasised. This argument is ignored if type="function" 
+#' @param lambda a number in [0,1] which specifies the extent to which topology (default, with lambda=0)  or branch lengths (lambda=1) are emphasised. This argument is ignored if \code{type="function"}.
 #' @param type logical which takes the inputs "\code{number}" (default) or "\code{function}". When \code{type="number"}, the entries of the output vector are numeric; when \code{type="function"} each entry is a function of lambda
 #'
 #' @importFrom combinat combn2
@@ -224,7 +224,7 @@ tree.vec <- cmpfun(tree.vec)
 #'
 #' @param tr1 an object of the class \code{phylo} 
 #' @param tr2 an object of the class \code{phylo} 
-#' @param lambda a number in [0,1] which specifies the extent to which topology (default, with lambda=0)  or branch lengths (lambda=1) are emphasised. This argument is ignored if type="function" 
+#' @param lambda a number in [0,1] which specifies the extent to which topology (default, with lambda=0)  or branch lengths (lambda=1) are emphasised. This argument is ignored if \code{type="function"}. 
 #' @param type logical which takes the inputs "\code{number}" (default) or "\code{function}". When \code{type="number"}, the output is numeric; when \code{type="function"} the output is a function of lambda
 #'
 #'
@@ -371,7 +371,7 @@ tree.dist <- cmpfun(tree.dist)
 #' @author  Michelle Kendall \email{michelle.louise.kendall@@gmail.com}
 #'
 #' @param trees an object of the class \code{multiPhylo} 
-#' @param lambda a number in [0,1] which specifies the extent to which topology (default, with lambda=0)  or branch lengths (lambda=1) are emphasised. This argument is ignored if type="function" 
+#' @param lambda a number in [0,1] which specifies the extent to which topology (default, with lambda=0)  or branch lengths (lambda=1) are emphasised. This argument is ignored if \code{type="function"}. 
 #' @param type logical which takes the inputs "\code{number}" (default) or "\code{function}". When \code{type="number"}, the output is a matrix where each entry is numeric; when \code{type="function"} the output is a matrix where each entry is a function of lambda
 #'
 #'
@@ -556,7 +556,7 @@ multi.dist <- cmpfun(multi.dist)
 #'
 #' @param trees an object of the class \code{multiPhylo} 
 #' @param likes a vector of weightings for the trees. Defaults to a vector of 1's so that all trees are equally weighted, but can be used to weight trees according to likelihood or other characteristics.
-#' @param lambda a number in [0,1] which specifies the extent to which topology (default, with lambda=0)  or branch lengths (lambda=1) are emphasised. This argument is ignored if type="function" 
+#' @param lambda a number in [0,1] which specifies the extent to which topology (default, with lambda=0)  or branch lengths (lambda=1) are emphasised. This argument is ignored if \code{type="function"}. 
 #' 
 #'
 #' @importFrom compiler cmpfun
