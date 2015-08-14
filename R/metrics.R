67f1f1ad95b3b5fa7a18af44c9d3265ebc10bdc7
Michelle Kendall <m.kendall@imperial.ac.uk>
2015-08-14 14:17:19 +0100

    Update metrics.R

--- R/metrics.R
+++ R/metrics.R
@@ -118,7 +118,7 @@ pen.edge.treematch <- cmpfun(pen.edge.treematch)
 #' @author Jacob Almagro-Garcia \email{nativecoder@@gmail.com}
 #'
 #' @useDynLib 
-#' @import Rcpp 
+#' @importFrom Rcpp cppFunction
 
 
 
