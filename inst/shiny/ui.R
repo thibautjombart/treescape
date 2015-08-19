ed7171d1c1bc00f18f1ccf509aec9056cfb4235e
Thibaut Jombart <thibautjombart@gmail.com>
2015-08-19 18:18:20 +0100

    adding img to UI

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -5,36 +5,42 @@ library(shiny)
 shinyUI(
     pageWithSidebar(
         ##  TITLE
-        headerPanel("treescape"),
+        headerPanel(
+            img(src="img/logo.png", height="160")
+            ),
 
         ## SIDE PANEL CONTENT
         sidebarPanel(
 
             ## choice of type of data source
             conditionalPanel(condition = "$('li.active a').first().html()!= 'Help'",
-                             radioButtons("datatype", "What data source to use?",
+                             img(src="img/line.png", width="100%"),
+                             h2(HTML('<font color="#6C6CC4" size="6"> > Input </font>')),
+                             radioButtons("datatype", HTML('<font size="4"> Choose data source:</font>'),
                                           list("Example from treescape"="expl","Input file"="file"))),
 
             ## choice of dataset if source is an example
             conditionalPanel(condition = "input.datatype=='expl'&& $('li.active a').first().html()!= 'Help'",
-                             selectInput("dataset", "Select an example dataset:",
+                             selectInput("dataset",
+                                         HTML('<font size="4"> Select an example dataset:</font>'),
                                          choices=c("woodmiceTrees"))
                              ),
 
             ## choice of dataset if source is a file
             conditionalPanel(condition = "input.datatype=='file'&& $('li.active a').first().html()!= 'Help'",
-                             fileInput("datafile", p(HTML(' <font size="5"> Choose input file:</font>'), br(),
+                             fileInput("datafile", p(HTML(' <font size="4"> Choose input file:</font>'), br(),
                                                      strong("accepted formats:"), br(),
                                                      em("- multiphylo"), "saved from R (.RData/.Rda)", br(),
                                                      em("- nexus"), "file (.nex/.nexus)")
-                                       ),
-                             tags$hr()
+                                       )
                              ),
 
 
             conditionalPanel(
                 ## condition
                 "$('li.active a').first().html()!= 'Help'",
+                img(src="img/line.png", width="100%"),
+                h2(HTML('<font color="#6C6CC4" size="6"> > Options </font>')),
                 selectInput("treemethod", "Choose a tree summary:",
                             choices=c(
                             "Metric" = "metric",
@@ -69,7 +75,9 @@ shinyUI(
                 ## condition
                 "$('li.active a').first().html()==='Scatterplot'",
 
-                h3("Aesthetics"),
+                img(src="img/line.png", width="100%"),
+
+                h2(HTML('<font color="#6C6CC4" size="6"> > Aesthetics </font>')),
 
                 ## symbol size
                 sliderInput("pointsize", "Size of the points", value=1, min=0, max=10, step=0.2),
