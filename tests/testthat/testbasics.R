7c2c9156443e7e853340a5588272e3b454faf3b5
Michelle Kendall <m.kendall@imperial.ac.uk>
2015-08-21 17:03:47 +0100

    Further tests

--- tests/testthat/testbasics.R
+++ tests/testthat/testbasics.R
@@ -8,7 +8,8 @@ library(ape)
 
 tree_a <- rtree(100)
 tree_b <- rtree(100)
-trees <- rmtree(5,100)
+n <- 10 # number of trees for multiphylo object
+trees <- rmtree(n,100)
 l <- runif(1) # a random value for lambda
 
 ############################
@@ -38,6 +39,12 @@ test_that("tree.dist equals Euclidean distance between corresponding vectors", {
 test_that("tree.dist equals corresponding entry of multi.dist", {
   expect_equal(tree.dist(trees[[1]],trees[[2]]), multi.dist(trees)[[1]])
   })  
+  
+test_that("med.tree results are consistent with tree.vec", {
+  geom <- med.tree(trees)
+  expect_equal(geom$mindist,sqrt(sum((geom$centre - tree.vec(trees[[geom$median[[1]]]]))^2))) # mindist, centre and median are internally consistent, and consistent with tree.vec
+  expect_equal(geom$mindist,geom$distances[[geom$median[[1]]]]) # mindist equals the entry in `distances' corresponding to the (first) median tree
+ })
 
 ############################
 # test that save_memory versions match non-save_memory versions
@@ -53,6 +60,48 @@ test_that("save_memory version of med.tree equals normal med.tree", {
   expect_equal(med.tree(trees,save_memory=TRUE)$centre, as.numeric(med.tree(trees)$centre))
   })  
   
-## To add: 
-# more tests for med.tree
-# expect warnings and errors
+############################
+# test for errors and warnings
+############################
+
+test_that("error is given if lambda is outside of [0,1]", {
+  expect_error(tree.vec(tree_a,-1))
+  expect_error(tree.vec(tree_a,2))
+  expect_error(tree.dist(tree_a,tree_b,-1))
+  expect_error(tree.dist(tree_a,tree_b,2))
+  expect_error(multi.dist(trees,-1))
+  expect_error(multi.dist(trees,2))
+  expect_error(med.tree(trees,-1))
+  expect_error(med.tree(trees,2))
+  })
+
+test_that("warning is given for the combination return_lambda_function=TRUE, save_memory=TRUE", {
+  expect_warning(multi.dist(trees,return_lambda_function=TRUE, save_memory=TRUE))
+  expect_warning(med.tree(trees,return_lambda_function=TRUE, save_memory=TRUE))
+  })
+  
+test_that("error is given if input is not of class phylo / multiphylo", {
+  expect_error(tree.vec(trees))
+  expect_error(tree.dist(trees))
+  expect_error(multi.dist(tree_a))
+  expect_error(med.tree(tree_a))
+  })
+
+test_that("warning is given if tree edge lengths are not defined, then they are set to 1", {
+  newicktree <- read.tree(text="((A,B),C);") # a tree without defined edge lengths
+  expect_warning(tree.vec(newicktree))
+  expect_equal(tree.vec(newicktree),c(1,0,0,1,1,1))
+  })  
+  
+test_that("error is given if trees have different tip labels", {
+  tree_c <- rtree(99)
+  tree_d <- tree_a
+  tree_d$tip.label <- 1:100 # note that tree_a has tip labels t1, t2, ...
+  expect_error(tree.dist(tree_a,tree_c))
+  expect_error(tree.dist(tree_a,tree_d))
+  })
+  
+test_that("error is given if weights vector is not of length n", {
+  expect_error(med.tree(trees,weights=rep(1,n+1)))
+  expect_error(med.tree(trees,weights=rep(1,n+1),return_lambda_function=TRUE))
+  })
