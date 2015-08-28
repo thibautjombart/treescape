5a1d230a14013f49fee4c44f96a42ff18a8e90f0
Thibaut Jombart <thibautjombart@gmail.com>
2015-08-28 12:13:55 +0100

    adding doc using roxygen2

---
+++ R/data.R
@@ -0,0 +1,36 @@
+#' Bootstrap trees from woodmouse dataset
+#'
+#' These trees were created using the neighbour-joining and bootstrapping
+#' example from the ape documentation.
+#'
+#'
+#' @name woodmiceTrees
+#' @docType data
+#' @format A multiPhylo object containing 201 trees, each with 15 tips
+#' @references Michaux, J. R., Magnanou, E., Paradis, E., Nieberding, C. and
+#' Libois, R. (2003) Mitochondrial phylogeography of the Woodmouse (Apodemus
+#' sylvaticus) in the Western Palearctic region. \emph{Molecular Ecology}, 12,
+#' 685-697
+#' @source A set of 15 sequences of the mitochondrial gene cytochrome b of the
+#' woodmouse (Apodemus sylvaticus) which is a subset of the data analysed by
+#' Michaux et al. (2003). The full data set is available through GenBank
+#' (accession numbers AJ511877 to AJ511987)
+#' @keywords datasets
+NULL
+
+
+#' BEAST analysis of seasonal influenza (A/H3N2)
+#'
+#' These trees were created using BEAST on hemagglutinin (HA) segments
+#' of seasonal influenza A/H3N2 samples collected in New-York city (US)  between 2000 and 2003. This data comes from the influenza BEAST tutorial distributed at:
+#' http://beast.bio.ed.ac.uk/tutorials
+#'
+#' Only the first 200 trees (out of 10,000) were retained.
+#'
+#' @name fluBeast
+#' @docType data
+#' @format A multiPhylo object containing 200 trees, each with 165 tips
+#' @references http://beast.bio.ed.ac.uk/tutorials
+#' @source http://beast.bio.ed.ac.uk/tutorials
+#' @keywords datasets
+NULL
